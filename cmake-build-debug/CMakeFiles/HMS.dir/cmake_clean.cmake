dccb5b61680393540b5c977f2b31d9f7c3fa27f4
Abdelrhman Abo-Elmagd <49569548+AboElmagd01@users.noreply.github.com>
2020-10-30 05:52:15 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/HMS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HMS.dir/main.cpp.o"
+  "HMS.exe"
+  "HMS.pdb"
+  "libHMS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
